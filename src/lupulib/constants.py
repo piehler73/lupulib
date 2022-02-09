4ee79167a7e5953a80d347e4985b1df40796074a
piehler73 <ahaagner@gmx.de>
2022-02-09 23:04:27 +0100

    updated

--- src/lupulib/constants.py
+++ src/lupulib/constants.py
@@ -2,7 +2,7 @@
 
 # Used in setup.py
 # -*- coding: utf-8 -*-
-VERSION = "0.2.4"
+VERSION = "0.2.5"
 PROJECT_PACKAGE_NAME = "lupulib"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupulib"
