2a929892f4a2cc47d1c01f47dfb6163b00fa937a
piehler73 <ahaagner@gmx.de>
2022-02-09 22:53:32 +0100

    changed version to 00.02.04

--- src/lupulib/constants.py
+++ src/lupulib/constants.py
@@ -2,7 +2,7 @@
 
 # Used in setup.py
 # -*- coding: utf-8 -*-
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 PROJECT_PACKAGE_NAME = "lupulib"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupulib"
