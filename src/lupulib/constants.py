1055aaa90baeaafeb3b3d9b2bb42d0ec51b79d90
piehler73 <ahaagner@gmx.de>
2022-02-09 15:47:04 +0100

    json handling fixed

--- src/lupulib/constants.py
+++ src/lupulib/constants.py
@@ -2,7 +2,7 @@
 
 # Used in setup.py
 # -*- coding: utf-8 -*-
-VERSION = "0.1.1"
+VERSION = "0.2.2"
 PROJECT_PACKAGE_NAME = "lupulib"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupulib"
