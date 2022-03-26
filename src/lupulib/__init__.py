68a78b0e6db8b483d8e39b1a46ae6df94d6db6f4
piehler73 <ahaagner@gmx.de>
2022-03-26 11:30:02 +0100

    issues fixed

--- src/lupulib/__init__.py
+++ src/lupulib/__init__.py
@@ -68,6 +68,7 @@ class LupusecAPI:
         self._auth = None
         if self._username != None and self._password != None:
             self._auth = aiohttp.BasicAuth(login=self._username, password=self._password, encoding='utf-8')
+            _LOGGER.debug("...auth.encode: %s", self._auth.decode())  
             _LOGGER.debug("...set aiohttp.BasicAuth")
         self._system = None
         self._token = None
--- src/lupulib/constants.py
+++ src/lupulib/constants.py
@@ -4,7 +4,7 @@
 # -*- coding: utf-8 -*-
 
 # Project Information
-VERSION = "0.3.45"
+VERSION = "0.3.46"
 PROJECT_PACKAGE_NAME = "lupulib"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupulib"
